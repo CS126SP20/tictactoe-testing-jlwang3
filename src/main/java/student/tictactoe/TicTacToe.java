57d0696bddcb2d844dead1121273cd3e037577f1
JeffreyWang <jlwang3@illinois.edu>
2020-01-29 13:56:54 -0600

    Commenting

--- src/main/java/student/tictactoe/TicTacToe.java
+++ src/main/java/student/tictactoe/TicTacToe.java
@@ -6,4 +6,8 @@ public class TicTacToe {
     public static Evaluation evaluateBoard(String boardState) {
         return Evaluation.NoWinner;
     }
+
+    // Invalid Input: Let X be number of Xs and O be number of Os. If X >= O + 2 or O > X, then the input is invalid.
+    // No Winner: The Board is filled up with 5 Xs and 4 Os but there exists no three-in-a-rows.
+    // X Wins: 
 }
