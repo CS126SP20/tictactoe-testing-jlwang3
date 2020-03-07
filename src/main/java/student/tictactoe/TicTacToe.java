65a3f8b3c161144ef776ebba2518c9dffc447b24
JeffreyWang <jlwang3@illinois.edu>
2020-03-07 11:33:12 -0600

    Final

--- src/main/java/student/tictactoe/TicTacToe.java
+++ src/main/java/student/tictactoe/TicTacToe.java
@@ -2,123 +2,95 @@ package student.tictactoe;
 
 import student.tictactoe.Evaluation;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class TicTacToe {
+
+    private static String board;
+    private static int boardSize = 3;
+    private static int boardLength = 9;
+
     public static Evaluation evaluateBoard(String boardState) {
-        // Assume the String is a reachable state for now.
-        // First break String into array of Chars.
-        char[] BOARD = new char[9];
-        BOARD = boardState.toCharArray();
-        // Define number of X and number of O;
-        int numberX = 0;
-        int numberO = 0;
-        for (int i = 0; i < 9; i++) {
-            if (BOARD[i] == 'x' || BOARD[i] == 'X') {
-                numberX++;
-            }
-            if (BOARD[i] == 'o' || BOARD[i] == 'O') {
-                numberO++;
-            }
-        }
-        // 1.
-        if (numberX >= numberO + 2 || numberO > numberX) {
+        if (boardState.length() != 9) {
             return Evaluation.InvalidInput;
         }
-        // 2. X wins and O Wins on Board
-        if ((((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X'))
-                || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X'))
-                || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-                || ((BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X'))
-                || ((BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
 
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X')))
-        && (((BOARD[0] == 'o' || BOARD[0] == 'O') && (BOARD[1] == 'o' || BOARD[1] == 'O') && (BOARD[2] == 'o' || BOARD[2] == 'O'))
-                || ((BOARD[3] == 'o' || BOARD[3] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[5] == 'o' || BOARD[5] == 'O'))
-                || ((BOARD[6] == 'o' || BOARD[6] == 'O') && (BOARD[7] == 'o' || BOARD[7] == 'O') && (BOARD[8] == 'o' || BOARD[8] == 'O'))
-                || ((BOARD[0] == 'o' || BOARD[0] == 'O') && (BOARD[3] == 'o' || BOARD[3] == 'O') && (BOARD[6] == 'o' || BOARD[6] == 'O'))
-                || ((BOARD[1] == 'o' || BOARD[1] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[7] == 'o' || BOARD[7] == 'O'))
-                || ((BOARD[2] == 'o' || BOARD[2] == 'O') && (BOARD[5] == 'o' || BOARD[5] == 'O') && (BOARD[8] == 'o' || BOARD[8] == 'O'))
+        board = boardState.toUpperCase();
 
-                || ((BOARD[0] == 'o' || BOARD[0] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[8] == 'o' || BOARD[8] == 'O'))
-                || ((BOARD[2] == 'o' || BOARD[2] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[6] == 'o' || BOARD[6] == 'O'))
-        ))
-        {
+        if (isUnreachableState()) {
             return Evaluation.UnreachableState;
         }
-        // 2. Configurations in which X "wins twice"
-        // Note: There are no such configurations for O since O has only 4 moves
-        // In Order of rows, columns, and diagonals
-        if (((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X'))
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-            || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-                || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X'))
-                || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-            || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[3] == 'x' || BOARD[3] == 'X'))
-                || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X'))
-                || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X'))
-                || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X'))
-                || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X'))
-            || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X'))
-                || ((BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-                || ((BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X'))
-                || ((BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-        )
-        {
-            return Evaluation.UnreachableState;
-        }
-        // Check for X win by checking all rows, columns, and diagonals
-        if (((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[2] == 'x' || BOARD[2] == 'X'))
-                || ((BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X'))
-                || ((BOARD[6] == 'x' || BOARD[6] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-            || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[3] == 'x' || BOARD[3] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-                || ((BOARD[1] == 'x' || BOARD[1] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[7] == 'x' || BOARD[7] == 'X'))
-                || ((BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[5] == 'x' || BOARD[5] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-
-                || ((BOARD[0] == 'x' || BOARD[0] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[8] == 'x' || BOARD[8] == 'X'))
-                || ((BOARD[2] == 'x' || BOARD[2] == 'X') && (BOARD[4] == 'x' || BOARD[4] == 'X') && (BOARD[6] == 'x' || BOARD[6] == 'X'))
-               )
-        {
-            if (numberX == numberO) {
-                return Evaluation.UnreachableState;
-            }
-            return Evaluation.Xwins;
-        }
-        // Check for O win by checking all rows, columns, and diagonals
-        if (((BOARD[0] == 'o' || BOARD[0] == 'O') && (BOARD[1] == 'o' || BOARD[1] == 'O') && (BOARD[2] == 'o' || BOARD[2] == 'O'))
-                || ((BOARD[3] == 'o' || BOARD[3] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[5] == 'o' || BOARD[5] == 'O'))
-                || ((BOARD[6] == 'o' || BOARD[6] == 'O') && (BOARD[7] == 'o' || BOARD[7] == 'O') && (BOARD[8] == 'o' || BOARD[8] == 'O'))
-                || ((BOARD[0] == 'o' || BOARD[0] == 'O') && (BOARD[3] == 'o' || BOARD[3] == 'O') && (BOARD[6] == 'o' || BOARD[6] == 'O'))
-                || ((BOARD[1] == 'o' || BOARD[1] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[7] == 'o' || BOARD[7] == 'O'))
-                || ((BOARD[2] == 'o' || BOARD[2] == 'O') && (BOARD[5] == 'o' || BOARD[5] == 'O') && (BOARD[8] == 'o' || BOARD[8] == 'O'))
-
-                || ((BOARD[0] == 'o' || BOARD[0] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[8] == 'o' || BOARD[8] == 'O'))
-                || ((BOARD[2] == 'o' || BOARD[2] == 'O') && (BOARD[4] == 'o' || BOARD[4] == 'O') && (BOARD[6] == 'o' || BOARD[6] == 'O'))
-        )
-        {
-            if (numberX == numberO + 1) {
-                return Evaluation.UnreachableState;
-            }
-            return Evaluation.Owins;
-        }
-    // Else... No Winner
-        return Evaluation.NoWinner;
     }
 
-    // 1. Invalid Input: Let X be number of Xs and O be number of Os. If X >= O + 2 or O > X, then the input is invalid.
-    // 5. No Winner: The Board is filled up with 5 Xs and 4 Os but there exists no three-in-a-rows.
-    // 3. X Wins:
-    // 4. O Wins:
-    // 2. Unreachable State: If both X and O win, then the state is unreachable. Is that the only Unreachable State?
-    // Note: Invalid Inputs eliminate many Unreachable State cases.
-    // X can win with the board filled completely.
-    // Questions: Are there any Unreachable States where the board is not filled?
-    // What are the Boundary Cases?
+    /**
+     * This method checks for three in a row.
+     * @return A map of X's and O's.
+     */
+    private static Map<Character, Integer> threeInARow() {
+
+        Map freqOfXO = new HashMap();
+
+        for (int i = 0; i < boardSize; i++) {
+            if (board.charAt(boardSize * i) == 'X' && board.charAt(boardSize * i + 1) == 'X' && board.charAt(boardSize * i + 2) == 'X') {
+                freqOfXO.put('X', (int) freqOfXO.get('X') + 1);
+            }
+            if (board.charAt(boardSize * i) == 'O' && board.charAt(boardSize * i + 1) == 'O' && board.charAt(boardSize * i + 2) == 'O') {
+                freqOfXO.put('O', (int) freqOfXO.get('O') + 1);
+            }
+        }
+
+        for (int i = 0; i < boardSize; i++) {
+            if (board.charAt(boardSize * 0 + i) == 'X' && board.charAt(boardSize * 1 + i) == 'X' && board.charAt(boardSize * 2 + i) == 'X') {
+                freqOfXO.put('X', (int) freqOfXO.get('X') + 1);
+            }
+            if (board.charAt(boardSize * 0 + i) == 'O' && board.charAt() == 'O' && board.charAt(boardSize * 2 + i) == 'O') {
+                freqOfXO.put('O', (int) freqOfXO.get('O') + 1);
+            }
+        }
+
+        if ((board.charAt(0) == 'X' && board.charAt(boardLength / 2) == 'X' && board.charAt(boardLength - 1) == 'X')
+                || (board.charAt(boardSize - 1) == 'X' && board.charAt(boardLength / 2) == 'X' && board.charAt(boardSize * (boardSize - 1)) == 'X')) {
+            freqOfXO.put('X', (int) freqOfXO.get('X') + 1);
+        }
+        if ((board.charAt(0) == 'O' && board.charAt(boardLength / 2) == 'O' && board.charAt(boardLength - 1) == 'O')
+                || (board.charAt(boardSize - 1) == 'O' && board.charAt(boardLength / 2) == 'O' && board.charAt(boardSize * (boardSize - 1)) == 'O')) {
+            freqOfXO.put('O', (int) freqOfXO.get('O') + 1);
+        }
+    }
+
+    /**
+     * This method determines whether or not the board is an unreachable state using knowledge of how many X and O's there are.
+     * Returns a boolean.
+     */
+    private static boolean isUnreachableState() {
+        //https://stackoverflow.com/questions/3867890/count-character-occurrences-in-a-string-in-c
+        int x = 0;
+        int o = 0;
+
+        for (int i = 0; i < boardLength; i++) {
+            if (board.charAt(i) == 'X') {
+                x++;
+            }
+            if (board.charAt(i) == 'O') {
+                o++;
+            }
+        }
+
+        if (x != o && x != o + 1) {
+            return true;
+        }
+        if (x == o && threeInARow().get('X') > 0) {
+            return true;
+        }
+        if (x == o + 1 && threeInARow().get('O') > 0) {
+            return true;
+        }
+        if (threeInARow().get('X') > 1 || threeInARow().get('O') > 1) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/student/tictactoe/TicTacToeTest.java
+++ src/test/java/student/tictactoe/TicTacToeTest.java
@@ -8,8 +8,59 @@ import static org.junit.Assert.*;
 
 
 public class TicTacToeTest {
+
     @Test
-    public void simpleNoWinnerBoard() throws Exception {
-        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("O...X.X.."));
+    public void NoWinnerBoard() throws Exception {
+        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("xxoooxxxo"));
+    }
+
+    @Test
+    public void NoWinnerBoard1() throws Exception {
+        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("xo......."));
+    }
+
+    @Test
+    public void XWinsBoard() throws Exception {
+        assertEquals(Evaluation.Xwins, TicTacToe.evaluateBoard("xO..xo..x"));
+    }
+
+    @Test
+    public void XWinsBoard1() throws Exception {
+        assertEquals(Evaluation.Xwins, TicTacToe.evaluateBoard("xxxo.o..."));
+    }
+
+    @Test
+    public void OWinsBoard() throws Exception {
+        assertEquals(Evaluation.Owins, TicTacToe.evaluateBoard("XXO.XO.O.."));
+    }
+
+    @Test
+    public void OWinsBoard1() throws Exception {
+        assertEquals(Evaluation.Owins, TicTacToe.evaluateBoard("oOo.x.x.x"));
+    }
+
+    @Test
+    public void UnreachableBoard() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("xo..xo.ox"));
+    }
+
+    @Test
+    public void UnreachableBoard1() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("xxxoxooox"));
+    }
+
+    @Test
+    public void UnreachableBoard2() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("x..x.x.xO"));
+    }
+
+    @Test
+    public void UnreachableBoard3() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("xoxxo..ox"));
+    }
+
+    @Test
+    public void InvalidBoard() throws Exception {
+        assertEquals(Evaluation.InvalidInput, TicTacToe.evaluateBoard("XXO.XO.O..-"));
     }
 }
